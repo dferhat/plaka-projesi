15e65f09cec6f076f34b9f54b6523c112812ea83
dferhat <fdemirr8@gmail.com>
2019-04-28 22:22:05 +0300

    takım sayfası oluşturuldu

---
+++ src/team.js
@@ -0,0 +1,18 @@
+var left = document.querySelector(".first");
+var right = document.querySelector(".second");
+
+left.addEventListener("mouseenter", () => {
+    left.classList.add("mouseenter-left");
+});
+
+left.addEventListener("mouseleave", () => {
+    left.classList.remove("mouseenter-left");
+});
+
+right.addEventListener("mouseenter", () => {
+    right.classList.add("mouseenter-right");
+});
+
+right.addEventListener("mouseleave", () => {
+    right.classList.remove("mouseenter-right");
+});
